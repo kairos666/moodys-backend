abdbcc051f46f9304fbce42d117a99906198e323
Unknown <musashi.miyamoto81@gmail.com>
2017-11-23 23:18:38 +0100

    [hook] basic notif object validation

--- src/hooks/notification-validator.js
+++ src/hooks/notification-validator.js
@@ -1,23 +1,49 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 const logger = require('winston');
+const errors = require('feathers-errors'); 
+const Ajv = require('ajv');
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function notificationValidator (hook) {
     // Hooks can either return nothing or a promise
     // that resolves with the `hook` object for asynchronous operations
 
-    // notification object type check
-    // let notifScheme = {
-    //   title: 'string',
-    //   options: {
-    //     badge: 'relative url of an image - android minimized notif 96x96 PNG',
-    //     icon: 'relative url of an image - displayed inside notif 256x256 PNG',
-    //     body: 'string',
-    //     tag: 'string'
-    //   }
-    // }
-    logger.info('notification validation', hook.data);
-    return Promise.resolve(hook);
+    // Schemes
+    const Notif = {
+      $id: 'Notif',
+      properties: {
+        title: { type: 'string' },
+        options: { $ref: 'NotifOptions', type: 'object' }
+      },
+      required: ['title']
+    }
+
+    const NotifOptions = {
+      $id: 'NotifOptions',
+      properties: {
+        badge: { type: 'string', format: 'uri-template' },
+        icon: { type: 'string', format: 'uri-template' },
+        body: { type: 'string' },
+        tag: { type: 'string' }
+      }
+    }
+
+    // init JSON schema validator (adding necessary sub schemes)
+    const ajv = new Ajv();
+    ajv.addSchema(NotifOptions);
+
+    // perform validation
+    if (ajv.validate(Notif, hook.data)) {
+      // validation success
+      logger.debug('notification schema validated', hook.data);
+      return Promise.resolve(hook);
+    } else {
+      // validation failed
+      hook.result = null; 
+      hook.error = new errors.NotAcceptable('data not valid - Notif Schema', ajv.errors);
+      logger.debug('notification schema invalid', hook.data, ajv.errors);
+      return Promise.reject(hook.error);
+    }
   };
 };
