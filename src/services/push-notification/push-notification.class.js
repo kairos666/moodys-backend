6618e72311df530c19684fc06d9d5c10d035bd1c
Unknown <musashi.miyamoto81@gmail.com>
2017-11-25 17:57:17 +0100

    [prototype] send successful push request to FCM
    based on simple web push demo subscription (local server)

--- src/services/push-notification/push-notification.class.js
+++ src/services/push-notification/push-notification.class.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-unused-vars */
+const axios = require('axios');
+const errors = require('feathers-errors'); 
+
 class Service {
   constructor (options) {
     this.options = options || {};
@@ -9,7 +12,22 @@ class Service {
       return Promise.all(data.map(current => this.create(current)));
     }
 
-    return Promise.resolve(data);
+    // send to FCM push service and handle response
+    return axios({
+      method: 'post',
+      url: `https://fcm.googleapis.com${data.path}`,
+      headers: {
+        'TTL': 60,
+        'Authorization': data.vapid
+      }
+    }).then(resp => {
+      return Promise.resolve({
+        status: resp.status,
+        statusText: resp.statusText
+      });
+    }).catch(error => {
+      return Promise.reject(new errors.GeneralError('FCM error'));
+    });
   }
 }
 
