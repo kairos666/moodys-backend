4a64d5b3d4a60f7650631157be99a825fe85abcc
Unknown <musashi.miyamoto81@gmail.com>
2017-12-11 22:06:44 +0100

    [hook] moved authentication to the service level (not app level)
    necessary for fine grain control (avoid checking subscriptions service)

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,11 +1,10 @@
 // Application hooks that run for every service
 const logger = require('./hooks/logger');
-const apiAuthenticate = require('./hooks/api-authenticate');
 const errorHandler = require('./hooks/error-handler');
 
 module.exports = {
   before: {
-    all: [ apiAuthenticate() ],
+    all: [],
     find: [],
     get: [],
     create: [],
--- src/services/push-notification/push-notification.hooks.js
+++ src/services/push-notification/push-notification.hooks.js
@@ -1,10 +1,10 @@
 
-
+const apiAuthenticate = require('../../hooks/api-authenticate');
 const notificationValidator = require('../../hooks/notification-validator');
 
 module.exports = {
   before: {
-    all: [],
+    all: [ apiAuthenticate() ],
     find: [],
     get: [],
     create: [ notificationValidator() ],
